fb11077bb90aaac2ac53c96661fffc143ca1293b
SharathVasan <sharathvasan99@gmail.com>
2024-06-28 00:11:59 +0530

    User Authentication completed with convex and clerk

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useConvex, useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
-import React from "react";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+import { SignInButton } from "@clerk/clerk-react";
 
 const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -14,9 +18,23 @@ const Heading = () => {
       <h3 className="text-base sm:text-xl md:text-wxl font-medium">
         NoteIt is the connected workspace where better and faster work happens.
       </h3>
-      <Button>
-        Enter NoteIt <ArrowRight className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter NoteIt <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>Get NoteIt free</Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://prime-oyster-84.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
