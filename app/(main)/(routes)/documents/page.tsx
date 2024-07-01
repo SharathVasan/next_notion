7b1dc69329ae740c31a69239392065e04b4847d2
SharathVasan <sharathvasan99@gmail.com>
2024-07-01 21:57:33 +0530

    Complete default screen inside documents

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
 
 const DocumentsPage = () => {
-  return <div>Documents Page</div>;
+  const { user } = useUser();
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image src="/empty.svg" height={300} width="300" alt="Empty" />
+      <h2 className="text-lg font medium">
+        Welcome to {user?.firstName}&apos;s NoteIt
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a Note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsPage;
