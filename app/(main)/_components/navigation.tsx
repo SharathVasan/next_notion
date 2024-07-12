58b76092f463fd39b9924509080b783fa3c02c92
Sharath Vasan <75443379+SharathVasan@users.noreply.github.com>
2024-07-12 23:44:29 +0530

    Add change

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -121,7 +121,7 @@ const Navigation = () => {
     const promise = create({ title: "Untitled" });
     toast.promise(promise, {
       loading: "Creating a new note...",
-      success: "New note created!",
+      success: "New note created!!",
       error: "Failed to create a new note.",
     });
   };
